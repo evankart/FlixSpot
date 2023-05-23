763c038e70d0e6199e54844fd07474ae9ea47643
Evan Kartheiser <ekarthei@gmail.com>
2023-05-23 11:48:55 -0700

    test deploy to backend

--- src/services/flowers.js
+++ src/services/flowers.js
@@ -1,32 +1,33 @@
 import axios from "axios";
 
 // const base = "http://localhost:5000";
-const base = "https://flowermap-backend.onrender.com";
+// const base = "https://flowermap-backend.onrender.com";
+const base = "https://flowermap-backend.onrender.com/api/v1/flowers";
 
 class FlowerDataService {
   getAll(page = 0) {
-    return axios.get(`${base}/api/v1/flowers?page=${page}`);
-    // return axios.get(`https://flowermap-backend.onrender.com/api/v1/flowers?page=${page}`);
+    return axios.get(`${base}?page=${page}`);
+    // return axios.get(`https://flowermap-backend.onrender.com?page=${page}`);
   }
   get(id) {
-    return axios.get(`${base}/api/v1/flowers/id/${id}`);
+    return axios.get(`${base}/id/${id}`);
   }
   find(query, by = "title", page = 0) {
-    return axios.get(`${base}/api/v1/flowers?${by}=${query}&page=${page}`);
+    return axios.get(`${base}?${by}=${query}&page=${page}`);
   }
   createReview(data) {
-    return axios.post(`${base}/api/v1/flowers/review`, data);
+    return axios.post(`${base}/review`, data);
   }
   updateReview(data) {
-    return axios.put(`${base}/api/v1/flowers/review`, data);
+    return axios.put(`${base}/review`, data);
   }
   deleteReview(id, userId) {
-    return axios.delete(`${base}/api/v1/flowers/review`, {
+    return axios.delete(`${base}/review`, {
       data: { review_id: id, user_id: userId },
     });
   }
   getRatings() {
-    return axios.get(`${base}/api/v1/flowers/ratings`);
+    return axios.get(`${base}/ratings`);
   }
 }
 
