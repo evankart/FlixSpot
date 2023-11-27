acd32fd4b4dd77a125bc515c723b6251e47cff31
Evan <ekarthei@gmail.com>
2023-11-27 14:03:08 -0800

    changed backend deployment from render to vercel https://flixspot-backend.vercel.app

--- src/services/flowers.js
+++ src/services/flowers.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 // const base = "http://localhost:5000/api/vi/flowers";
 // const base = "https://flowermap-backend.onrender.com";
-const base = "https://flowermap-backend.onrender.com/api/v1/flowers";
+// const base = "https://flowermap-backend.onrender.com/api/v1/flowers";
+const base = "https://flixspot-backend.vercel.app/api/v1/flowers";
 
 class FlowerDataService {
   getAll(page = 0) {
